88ff66ca706f8883b70f287e0f0cc9174b68333d
dev-gawe <gfpazzanese@gmail.com>
2026-02-04 19:32:31 -0300

    Publish version :D First release

--- fsm_entities/sloth/sloth.states.gd
+++
@@ -1,7 +0,0 @@
-extends FiniteStateMachine
-class_name SlothStates
-
-var STATE_IDLE : StateSlothIdle = StateSlothIdle.new(self)
-var STATE_REST : StateSlothRest = StateSlothRest.new(self)
-var STATE_WALK : StateSlothWalk = StateSlothWalk.new(self)
-var STATE_CLIMB : StateSlothClimb = StateSlothClimb.new(self)
